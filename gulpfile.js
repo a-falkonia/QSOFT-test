621b07ddb9c8eed7b1e9c15b1fd40c9ea42d3ded
Anna <a.falkonia@gmail.com>
2022-06-05 14:06:52 +0400

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,56 @@
+'use strict';
+
+var gulp = require('gulp');
+var plumber = require('gulp-plumber');
+var sourcemap = require('gulp-sourcemaps');
+var sass = require('gulp-sass')(require('sass'));
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var csso = require('gulp-csso');
+var rename = require('gulp-rename');
+var server = require("browser-sync").create();
+
+gulp.task('scss', function () {
+  return gulp
+    .src('app/sass/style.scss')
+    .pipe(plumber())
+    .pipe(sourcemap.init())
+    .pipe(sass())
+    .pipe(postcss([autoprefixer()]))
+    .pipe(csso())
+    .pipe(rename('style.min.css'))
+    .pipe(sourcemap.write('.'))
+    .pipe(gulp.dest('build/css'))
+    .pipe(server.stream());
+});
+
+gulp.task('html', function () {
+  return gulp.src('app/*.html').pipe(gulp.dest('build'));
+});
+
+gulp.task('images', function () {
+  return gulp
+    .src('app/img/**/*.{png,jpg,svg}')
+    .pipe(gulp.dest('build/img'));
+});
+
+gulp.task('server', function () {
+  server.init({
+    server: 'build/',
+    notify: false,
+    open: true,
+    cors: true,
+    ui: false,
+  });
+
+  gulp.watch('app/sass/**/*.{scss,sass}', gulp.series('scss'));
+  gulp.watch('app/*.html', gulp.series('html', 'refresh'));
+});
+
+gulp.task('refresh', function (done) {
+  server.reload();
+  done();
+});
+
+gulp.task('build', gulp.series('scss', 'html', 'images'));
+gulp.task('start', gulp.series('build', 'server'));
